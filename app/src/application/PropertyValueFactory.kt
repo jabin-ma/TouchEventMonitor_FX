c2a97fab8ce91ae195ae69505399adf60c55dc5a
majipeng <1402152841@qq.com>
2017-06-07 23:58:42 +0800

    fix ui

---
+++ app/src/application/PropertyValueFactory.kt
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2011, 2014, Oracle and/or its affiliates. All rights reserved.
+ * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+package application
+
+import javafx.beans.NamedArg
+import javafx.beans.property.Property
+import javafx.beans.property.ReadOnlyObjectWrapper
+import javafx.beans.value.ObservableValue
+import javafx.scene.control.TableCell
+import javafx.scene.control.TableColumn
+import javafx.scene.control.TableColumn.CellDataFeatures
+import javafx.scene.control.TableView
+import javafx.util.Callback
+
+import sun.util.logging.PlatformLogger
+import sun.util.logging.PlatformLogger.Level
+import com.sun.javafx.property.PropertyReference
+import com.sun.javafx.scene.control.Logging
+
+
+class PropertyValueFactory(@param:NamedArg("property") val property: String) : Callback<TableColumn.CellDataFeatures<EventData, out Any>, ObservableValue<out Any>>{
+
+    private var columnClass: Class<*>? = null
+    private var previousProperty: String? = null
+    private var propertyRef: PropertyReference<Any>? = null
+
+    override fun call(param: CellDataFeatures<EventData, out Any>?): ObservableValue<Any>? {
+        return getCellDataReflectively(param?.value)
+    }
+
+    private fun getCellDataReflectively(rowData: EventData?): ObservableValue<Any>? {
+        if (property == null || property.isEmpty() || rowData == null) return null
+
+        try {
+            // we attempt to cache the property reference here, as otherwise
+            // performance suffers when working in large data models. For
+            // a bit of reference, refer to RT-13937.
+            if (columnClass == null || previousProperty == null ||
+                    columnClass != rowData.javaClass ||
+                    previousProperty != property) {
+
+                // create a new PropertyReference
+                this.columnClass = rowData.javaClass
+                this.previousProperty = property
+                this.propertyRef = PropertyReference<Any>(rowData.javaClass, property)
+            }
+
+            if (propertyRef!!.hasProperty()) {
+                return propertyRef!!.getProperty(rowData)
+            } else {
+                val value = propertyRef!!.get(rowData)
+                return ReadOnlyObjectWrapper(value)
+            }
+        } catch (e: IllegalStateException) {
+            // log the warning and move on
+            val logger = Logging.getControlsLogger()
+            if (logger.isLoggable(Level.WARNING)) {
+                logger.finest("Can not retrieve property '" + property +
+                        "' in PropertyValueFactory: " + this +
+                        " with provided class type: " + rowData.javaClass, e)
+            }
+        }
+
+        return null
+    }
+}
--- app/src/application/controller/EventController.java
+++ app/src/application/controller/EventController.java
@@ -1,64 +1,60 @@
 package application.controller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import com.android.ddmlib.input.TouchEventObserver;
-
 import application.EventData;
+import com.android.ddmlib.input.TouchEventObserver;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.ToolBar;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.ProgressBarTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class EventController implements Initializable {
-	@FXML
-	MenuBar menubar_main;
-	@FXML
-	Menu menu_file, menu_edit, menu_help;
-	@FXML
-	MenuItem menuitem_close, menuitem_delete, menuitem_about;
+    @FXML
+    MenuBar menubar_main;
+    @FXML
+    Menu menu_file, menu_edit, menu_help;
+    @FXML
+    MenuItem menuitem_close, menuitem_delete, menuitem_about;
 
-	@FXML
-	ToolBar toolbar_main;
-	@FXML
-	Button toolbtn_first;
-	@FXML
-	Pane panel_table;
-	@FXML
-	TableView<EventData> tableview_events;
+    @FXML
+    ToolBar toolbar_main;
+    @FXML
+    Button toolbtn_start,toolbtn_pause,toolbtn_stop,toolbtn_replay;
+    @FXML
+    Pane panel_table;
+    @FXML
+    TableView tableview_events;
 
-	private TouchEventObserver mEventObserver;
+    private TouchEventObserver mEventObserver;
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	@Override
-	public void initialize(URL url, ResourceBundle rb) {
-		ObservableList<TableColumn<EventData,?>> observableList = tableview_events.getColumns();
-		observableList.get(0).setCellValueFactory(new PropertyValueFactory("eventType"));
-		observableList.get(1).setCellValueFactory(new PropertyValueFactory("eventDesc"));
-		observableList.get(2).setCellValueFactory(new PropertyValueFactory("eventDur"));
-		observableList.get(3).setCellValueFactory(new PropertyValueFactory("progress"));
-		ObservableList<EventData> data = FXCollections.observableArrayList();
-		data.add(new EventData("type", "desc", "dur", 10));
-		tableview_events.setItems(data);
-	}
-	
-	
-	
-	
-	
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        ObservableList<TableColumn> observableList = tableview_events.getColumns();
+        observableList.get(0).setCellValueFactory(new PropertyValueFactory("eventType"));
+        observableList.get(1).setCellValueFactory(new PropertyValueFactory("eventDesc"));
+        observableList.get(2).setCellValueFactory(new PropertyValueFactory("eventDur"));
+        observableList.get(3).setCellValueFactory(new PropertyValueFactory("progress"));
+        observableList.get(3).setCellFactory(ProgressBarTableCell.forTableColumn());
+        ObservableList<EventData> data = FXCollections.observableArrayList();
+        for (int i = 0; i < 1000; i++) {
+            data.add(new EventData("type", "desc", "dur", i/1000.00));
+        }
+        tableview_events.setItems(data);
+    }
 
-	public void testButton(ActionEvent event) {
+    public void onMenuClick(ActionEvent ev){
+        System.out.println(ev);
+    }
 
-	}
+    public void onToolBtnClick(ActionEvent ev){
+        System.out.println(ev);
+    }
 }
