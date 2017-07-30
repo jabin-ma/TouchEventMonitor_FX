b4e488cff3bc3f27024d185ccfe97e75bed3e424
Ash1 <1402152841@qq.com>
2017-07-30 21:36:16 +0800

    remove cmd-SENDEVENT

--- ddmlib/src/main/java/com/android/ddmlib/input/Command.java
+++ ddmlib/src/main/java/com/android/ddmlib/input/Command.java
@@ -5,7 +5,7 @@ public enum Command {
     GETEVENT("设备输入设备", "getevent"), GETEVENT_GETDEVICE("获取输入设备", GETEVENT, "-p"), GETEVENT_WHATCH_TEXT_EVENT("监听输入",
             GETEVENT, "-lt"), GETEVENT_WHATCH_RAW_EVENT("监听输入", GETEVENT, "-t"),
 
-    SENDEVENT("发送输入时间", "sendevent"),
+//    SENDEVENT("发送输入时间", "sendevent"),
 
     INPUT("发送指令", "input"), INPUT_TAP("点击", "input tap"), INPUT_LTAP("长按", "intput swipe"), INPUT_SWIPE("滑动",
             "intput swipe");
